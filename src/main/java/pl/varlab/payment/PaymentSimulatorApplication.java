80981f6c03588e26c147447f6b6894a02be17d18
var <adrian.lukawski@gmail.com>
2024-10-24 22:25:53 +0200

    spring initalizr project

---
+++ src/main/java/pl/varlab/payment/PaymentSimulatorApplication.java
@@ -0,0 +1,13 @@
+package pl.varlab.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSimulatorApplication.class, args);
+	}
+
+}
