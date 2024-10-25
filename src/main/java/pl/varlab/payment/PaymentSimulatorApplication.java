e4a706c751deccbc6ed529606f74edab1caf4af5
var <adrian.lukawski@gmail.com>
2024-10-25 17:55:08 +0200

    enable retry and task scheduling pool size threads

--- src/main/java/pl/varlab/payment/PaymentSimulatorApplication.java
+++ src/main/java/pl/varlab/payment/PaymentSimulatorApplication.java
@@ -2,8 +2,12 @@ package pl.varlab.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableRetry
 public class PaymentSimulatorApplication {
 
 	public static void main(String[] args) {
